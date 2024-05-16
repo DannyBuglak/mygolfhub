ea0d7ab14dbdbc7b164a81a8cdba04a0cd652c92
Danny Buglak <dannybuglak4@gmail.com>
2024-05-16 18:21:14 -0400

    Start to pages and components, router installed

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>MyGolfHub</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/track-statistics">Track Statistics</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to MyGolfHub</h2>
+      <p>Track your golf statistics easily and efficiently.</p>
+    </div>
+  );
+}
+
+export default Home;
